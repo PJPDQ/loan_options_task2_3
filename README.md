0016defa55f9e851e9f0541132a8f0bcd6d0c4f8
DickySG <june_dicky@hotmail.com>
2021-11-13 02:37:37 +1000

    added descending order for php implementation that was missing from previous version.

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # loan_options_task2_3
+
+## This is Coding Assessment Coding solution for Task 2 and 3.
+
+# Task 2
+
+- Task 2 is assessing applicant to create a PHP file to be executed in the command line prompt. 
+- The parameter provided by the user with at least 3 arguments consisting the category desired and number of data expected to display in command prompt as a standard output.
+
+For proper output execution, the program has been tested and executed through
+
+`php index.php <category> <limit>`
+
+where <category> should be replaced with the desired category and <limit> is the number of data to display.
+List of eligible <category>:
+* Animals
+* Anime
+* Blockchain
+* Books
+* Business
+* Calendar
+* Weather
+* Transportation
+* Health
+* Jobs
+* Music
+
+Returns: expected to be a list of API-descending-order dataset
+
+## Tested Environment
+
+For first time running PHP executable program, please add environment variable of php.
+
+executable php can be found using your XAMPP directory->php directory->php and add this into your list of environment variable $PATH for user variables and System Variables.
+
+`C:\xampp\php`
+
+# Task 3
+A provided tables named `customer` and `broker` that consists some prefilled non-null elements of customers and brokers respectively.
+The customer table consists of:
+* ID
+* NAME
+* AMOUNT
+* BROKER_ID
+
+The broker table consists of:
+* ID
+* NAME
+
+The broker.ID is equal to the customer.BROKER_ID. So, it is just a matter of joining two tables and count the number of customers each broker is handling.
+
+Returns: a virtual table of Broker_Name and Number_of_customers in descending order from the BrokerName and its count.
+
+
--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
+/* This is a function to get the number of Category Contents
+	Params: 
+			$category: desired category to display
+			$limit: positive number of category description to display
+	Returns:
+			none, prints out directly to standard out.
+*/
+function getCategoryContents($category, $limit) {
+	//Checking if limit is an positive integer
+	if ($limit < 1 or !ctype_digit(strval($limit))) {
+		print_r("Limit must be a positive integer.\n");
+		exit(-1);
 	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+	//GET data from API
+	$api_url = "https://api.publicapis.org/entries";
+	//Get the file content
+	$json_data = file_get_contents($api_url);
+	$response = json_decode($json_data); //Decode JSON
+	$category_data = $response->entries; //GET the list entries.
+	$x = 0; //Index number keeping track of limit
+	$out = array();
+	foreach ($category_data as $cat) {
+		if ($x == $limit) {
+			break;
+		}
+		if (strtolower($cat->Category) == strtolower($category)) {
+			//print_r($cat);
+			$temp = "API: ".$cat->API."\nDescription: ".$cat->Description."\nAuth: ".$cat->Auth."\nHTTPS: ".$cat->HTTPS."\nCors: ".$cat->Cors."\nLink: ".$cat->Link."\nCategory: ".$cat->Category."\n";
+			$out[$cat->API] = $temp;
+			$x += 1;
+		}
+	}
+	if ($x == 0) {
+		print_r("No results\n");
+	} else {
+		//Allow descending order based on the api as key
+		krsort($out);
+		foreach ($out as $api => $cat_val) {
+			echo $cat_val."\n"; //display the result to standard out
+		}
+	}
+}
+
+getCategoryContents($argv[1], $argv[2]);
 ?>
-Something is wrong with the XAMPP installation :-(
